91c5193b6255666dbfd39db7de94fba9c819b156
Aakash <editsky597@gmail.com>
2025-06-21 16:43:22 +0530

    basic questions

---
+++ basic questions.sql
@@ -0,0 +1,15 @@
+# 1. List all unique cities where customers are located.
+
+SELECT DISTINCT
+    customer_city
+FROM
+    customers;
+
+# 2. Count the number of orders placed in 2017.
+
+
+
+
+
+
+
